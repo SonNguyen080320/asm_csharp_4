2438aa49ff7d6a489e2e95202bbddb7577ee754a
Nguyen Trong Phuc <grayfptcs3@gmail.com>
2022-01-31 23:16:04 +0700

    add sign-in, sign-up

---
+++ asm_rewrite/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asm_rewrite.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("sign-in")]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [Route("sign-up")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
