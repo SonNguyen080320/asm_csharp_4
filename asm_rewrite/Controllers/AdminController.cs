ac1a8bc611a07247b9bba554fec2d06e11c6abfe
Nguyen Trong Phuc <grayfptcs3@gmail.com>
2022-01-31 20:41:45 +0700

    Add project files.

---
+++ asm_rewrite/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asm_rewrite.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Route("admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("admin/manage-products")]
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        [Route("admin/manage-categories")]
+        public IActionResult Categories()
+        {
+            return View();
+        }
+
+        [Route("admin/manage-orders")]
+        public IActionResult Orders()
+        {
+            return View();
+        }
+
+        [Route("admin/manage-customers")]
+        public IActionResult Customers()
+        {
+            return View();
+        }
+
+        [Route("admin/manage-users")]
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
---
+++ asm_rewrite/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using asm_rewrite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asm_rewrite.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/{id}")]
+        public IActionResult ProductDetail()
+        {
+            return View();
+        }
+    }
+}
